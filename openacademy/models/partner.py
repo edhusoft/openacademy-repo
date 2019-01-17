61c10457cd503ef4d16fa16fcea096f652c7c389
EdhuinCapcha <edhusoft@gmail.com>
2019-01-17 08:15:10 -0800

    [REF] openacademy: Add inherit matter

---
+++ openacademy/models/partner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+class Partner(models.Model):
+	_inherit = 'res.partner'
+
+	instructor = fields.Boolean(default=False)
+	session_ids = fields.Many2many('openacademy.session', string='Attend Sessions', readonly=True)
+	other_field = fields.Boolean(default=True)
+
